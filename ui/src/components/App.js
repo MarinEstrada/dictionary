ce251b0e6e0434927a12f59c01a868ad76ba4195
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-05-08 18:45:50 -0500

    Improve button appearence
    Also slight indentation fix

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -3,17 +3,17 @@ import './css/App.css';
 import SearchBox from './SearchBox';
 
 class App extends Component {
-        render(){ 
-  return (
-    <div className="App">
-      <header className="App-header">
-          <SearchBox
-          initialText="Welcome!"
-          />
-      </header>
-    </div>
-  );
-        }
+    render(){ 
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <SearchBox
+                        initialText="Welcome!"
+                    />
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
