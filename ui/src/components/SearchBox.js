36969b504454b515920bee59395c0db2b577a560
MarinEstrada <adrian.marin.estrada@gmail.com>
2021-05-07 23:36:57 -0500

    Add Button
    Also create SearchBox.css and got rid of React advertisement

--- ui/src/components/SearchBox.js
+++ ui/src/components/SearchBox.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import './css/SearchBox.css';
 
 class SearchBox extends Component {
         state = {
-                textValue: this.props.initialText
+                textValue: this.props.initialText,
+                tmpWord: "",
         }
         handleTextChange(newText){
                 this.setState(state => ({
                             ...state,
                         textValue: newText
+                }))
+        }
+        search(text){
+                this.setState(state => ({
+                        ...state,
+                        tmpWord: text
                 }))
         }
         render(){ 
@@ -18,6 +26,16 @@ class SearchBox extends Component {
           value={ this.state.textValue }
           onChange={ (event) => this.handleTextChange(event.target.value) }
           />
+          <div>
+              <div className="SearchButton">
+                      <p onClick={() => this.search(this.state.textValue)}>
+                      I'm a button
+                  </p>
+              </div>
+                      <p>
+                              {this.state.tmpWord}
+                      </p>
+          </div>
     </div>
   );
         }
